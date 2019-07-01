08ccf9b89dd5b553c4c8014f6a46c4d04c39f67f
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-01 17:04:12 +0430

    Add spring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,20 @@ import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
-import service.DbService;
-
+import in.nimbo.rssreader.service.DbService;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws IOException, FeedException {
-        String url = "https://www.isna.ir/rss";
-        SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
-        DbService dbService = new DbService();
-        dbService.addFeddToPostgres(feed);
-        System.out.println(feed.getEntries().get(0).getTitle());
+        for(int i = 0; i < 1000; i++) {
+            System.out.println(i);
+            try {
+                String url = "https://aftabnews.ir/fa/rss/" + i;
+                SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
+                DbService dbService = new DbService();
+                dbService.addFeedToPostgres(feed);
+            } catch (Exception ignored) {}
+        }
     }
 }
--- src/main/java/in/nimbo/rssreader/service/DbService.java
+++ src/main/java/in/nimbo/rssreader/service/DbService.java
@@ -1,40 +1,39 @@
-package service;
+package in.nimbo.rssreader.service;
 
-import com.rometools.rome.feed.atom.Feed;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
+import org.springframework.stereotype.Service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+@Service
 public class DbService {
 
     public static final String URL = "jdbc:postgresql://localhost:5432/news";
     public static final String USER = "postgres";
     public static final String PASSWORD = "1234";
-    public static final String addQuery = "INSERT INTO \"news\".\"news\" (\"title\",\"time\",\"description\",\"news_agency\",\"category\")\n" +
+    public static final String insertQuery = "INSERT INTO \"news\".\"news\" (\"title\",\"time\",\"description\",\"news_agency\",\"category\")\n" +
             "VALUES ('%s','%s','%s','%s','%s')";
 
-    public void addFeddToPostgres(SyndFeed feed) {
+    public void addFeedToPostgres(SyndFeed feed) {
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             Statement statement = connection.createStatement();
             for (SyndEntry entry : feed.getEntries()) {
                 try {
                     DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
                     String strDate = dateFormat.format(entry.getPublishedDate());
-                    statement.executeQuery(String.format(addQuery,
+                    String query = String.format(insertQuery,
                             entry.getTitle(),
                             strDate,
                             entry.getDescription().getValue(),
                             feed.getTitle(),
-                            entry.getCategories().isEmpty() ? entry.getCategories().get(0).getName() : ""));
+                            entry.getCategories().isEmpty() ? entry.getCategories().get(0).getName() : "");
+                    ResultSet result = statement.executeQuery(query);
                 } catch (Exception e) {
 //                    logger.info("news is not valid");
-                    //todo add logger
+                    // todo add logger
                 }
             }
         } catch (SQLException e) {
