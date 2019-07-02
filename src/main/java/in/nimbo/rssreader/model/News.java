ef89191aeefffc7478a8f0e4ef68e9bb485d40ed
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-02 10:15:15 +0430

    define news model

---
+++ src/main/java/in/nimbo/rssreader/model/News.java
@@ -0,0 +1,11 @@
+package in.nimbo.rssreader.model;
+
+import java.util.Date;
+
+public class News {
+    private String title;
+    private String description;
+    private String newsAgency;
+    private long date;
+
+}
